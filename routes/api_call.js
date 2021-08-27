380509b738322c2c4790e4742616bfcc63affc1e
Carsten Michel <carstenmichel@users.noreply.github.com>
2021-08-27 15:14:16 +0200

    add k8s secret

--- routes/api_call.js
+++ routes/api_call.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -10,6 +11,19 @@ router.get('/', function(req, res, next) {
     content= content +'<p>key: ' + key + '=' + env[key] +'</p>\n';
   });
 
+  content = content + "</BODY></HTML>";
+  res.send(content);
+
+});
+
+router.get('/secret', function(req, res, next) {
+  var content = "<HTML><BODY>";
+  try {
+    const data = fs.readFileSync('/etc/postgres-secret', 'utf8')
+    content = content + '<p>' +data +'</p>\n'
+  } catch (err) {
+    content = '<p>file not found</p>\n'
+  }
   content = content + "</BODY></HTML>";
   res.send(content);
 
