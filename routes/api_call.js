5bb19f2a19d01c2bff038381011d88157aba7769
Carsten Michel <carstenmichel@users.noreply.github.com>
2021-08-27 15:36:27 +0200

    secret

--- routes/api_call.js
+++ routes/api_call.js
@@ -5,19 +5,6 @@ var fs = require('fs')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   var content = "<HTML><BODY>";
-  var env = process.env;
-
-  Object.keys(env).forEach(function(key) {
-    content= content +'<p>key: ' + key + '=' + env[key] +'</p>\n';
-  });
-
-  content = content + "</BODY></HTML>";
-  res.send(content);
-
-});
-
-router.get('/secret', function(req, res, next) {
-  var content = "<HTML><BODY>";
   try {
     const data = fs.readFileSync('/etc/postgres-secret', 'utf8')
     content = content + '<p>' +data +'</p>\n'
